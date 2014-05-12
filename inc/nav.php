6fce8d3ce51e348e1416ae756fcbcf9f25abec15
gurieveugen <gurievinvest@gmail.com>
2014-05-12 15:07:07 +0300

    1.16

--- inc/nav.php
+++ inc/nav.php
@@ -22,7 +22,7 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
 		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
-		$output .= $indent . '<li' . $id . $value . $class_names .'><h2>';
+		$output .= $indent . '<li' . $id . $value . $class_names .'>';
 		$atts = array();
 		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
 		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
@@ -37,16 +37,18 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 			}
 		}
 		$item_output = $args->before;
+		if($depth==0) $item_output.= '<h2>';
 		$item_output.= '<a'. $attributes .'>';		
 		$item_output.= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 		$item_output.= '</a>';
+		if($depth==0) $item_output.= '</h2>';
 		$item_output.= $args->after;
 		$output     .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
 
 	function end_el( &$output, $item, $depth = 0, $args = array() ) 
 	{
-		$output .= "</h2></li>\n";
+		$output .= "</li>\n";
 	}
 }
 ?>
